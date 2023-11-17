44388581250110441e786b51220d6436392f60d0
Gabriel Hermenegildo <gabriel.f.hermenegildo@gmail.com>
2023-11-17 02:33:57 -0300

    adicionando marcador ao mapa

--- src/components/MapScreen.tsx
+++ src/components/MapScreen.tsx
@@ -1,24 +1,35 @@
-import React, { useEffect, useState } from "react";
-import { View } from "react-native";
-import MapView, {Marker} from "react-native-maps";
+import React, { useEffect, useState, useRef } from "react";
+import { View, TouchableOpacity } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 import { StyleSheet } from "react-native";
 import { Dimensions } from "react-native";
-
 import {
   requestForegroundPermissionsAsync,
   getCurrentPositionAsync,
+  watchPositionAsync,
   LocationObject,
+  LocationAccuracy,
 } from "expo-location";
 
 export default function MapScreen() {
   const [location, setLocation] = useState<LocationObject | null>(null);
+  const [markerCoordinate, setMarkerCoordinate] = useState<{
+    latitude: number;
+    longitude: number;
+  } | null>(null);
+
+  const mapRef = useRef<MapView>(null);
 
   async function requestLocationPermissions() {
     const { granted } = await requestForegroundPermissionsAsync();
 
     if (granted) {
       const currentPosition = await getCurrentPositionAsync({});
-      setLocation(currentPosition)
+      setLocation(currentPosition);
+      setMarkerCoordinate({
+        latitude: currentPosition.coords.latitude,
+        longitude: currentPosition.coords.longitude,
+      });
     }
   }
 
@@ -26,10 +37,44 @@ export default function MapScreen() {
     requestLocationPermissions();
   }, []);
 
+  useEffect(() => {
+    watchPositionAsync(
+      {
+        accuracy: LocationAccuracy.Highest,
+        timeInterval: 1000,
+        distanceInterval: 1,
+      },
+      (response) => {
+        setLocation(response);
+        setMarkerCoordinate({
+          latitude: response.coords.latitude,
+          longitude: response.coords.longitude,
+        });
+        mapRef.current?.animateCamera({
+          center: response.coords,
+        });
+      }
+    );
+  }, []);
+
+  const handleMapPress = (event: any) => {
+    const { coordinate } = event.nativeEvent;
+    setMarkerCoordinate(coordinate);
+    setLocation({
+      coords: {
+        latitude: coordinate.latitude,
+        longitude: coordinate.longitude,
+      },
+    });
+  };
+
+  console.log(location)
+
   return (
     <View style={styles.container}>
       {location && (
         <MapView
+          ref={mapRef}
           style={styles.map}
           initialRegion={{
             latitude: location.coords.latitude,
@@ -37,13 +82,15 @@ export default function MapScreen() {
             latitudeDelta: 0.005,
             longitudeDelta: 0.005,
           }}
+          onPress={handleMapPress}
         >
-           <Marker 
-                coordinate={{
-                    latitude: location.coords.latitude,
-                    longitude: location.coords.longitude,
-                }}
-           />
+          {markerCoordinate && (
+            <Marker
+              coordinate={markerCoordinate}
+              title="Você está aqui"
+              description="Sua localização atual"
+            />
+          )}
         </MapView>
       )}
     </View>
